5c3ca0cf1da57799b1ec618987d6e230ee278052
Mike Murphy <murph517@gmail.com>
2016-04-11 22:58:39 -0700

    Fixes Homework 1

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-var http = require("http")
-var fs = require("fs");
+var http = require ('http');
+var fs = require('fs');
 
-http.createServer(function(req,res){
-    var path = req.url.toLowerCase();
+function servePublicFile(res, path, contentType, responseCode) {
+	if(!responseCode) {responseCode = 200};
+	fs.readFile(__dirname + path, function (err,data){
+		if(err) {
+			res.writeHead(500, { 'Content-Type': 'text/plain'});
+			res.end('500 - Internal Error');
+			} else {
+				res.writeHead(responseCode, { 'Content-Type': 'text/plain'});
+				res.end(data);
+			}
+	});
+}
+
+http.createServer(function (req, res) {
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
     switch(path) {
-        case '/':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('HOMEPAGE');
+        case '':
+            servePublicFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About Page');
+			servePublicFile(res, '/public/about.html', 'text/html');
             break;
         default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not Found. Sorry, Invaild Route');
+			servePublicFile(res, '/public/404.html', 'text/html', 404);
             break;
     }
     
-}).listen(process.env.PORT);
-console.log("Server is up");
+}).listen(3000);
+console.log("Server is up at localhost:3000; To end press ctrl+c");
