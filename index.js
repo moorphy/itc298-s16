9e3352256e5b510f0d28db49b2d7893ccfe8bdba
Mike Murphy <murph517@gmail.com>
2016-06-05 00:10:29 +0000

    Homework 5. Routes and API

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
     var express = require('express');
     var server = express();
-    var books = require('./lib/books.js');
- 
+
     // CONFIGURE EXPRESS APP
     server.set('port', process.env.PORT || 3000);//SET PORT
     server.use(express.static(__dirname + '/public'));//CSS & IMG  
     server.use(require('body-parser').urlencoded({extended:true})); //support encoded bodies
-    
-     //Title Tag
-    var pageTitle = 'MM ITC298 Library';
+    //ROUTES
+    require('./routes.js')(server);
+    //CROSS ORIGIN RESOURCE SHARING
+    server.use('/api', require('cors')());    
+     
 
     //TEMPLATE ENGINE
     var viewsPath = __dirname + '/views'; 
@@ -22,96 +23,6 @@
     server.engine('hbs', handlebars.engine);
     server.set('views', viewsPath);
     server.set('view engine', 'hbs');
-
-
-    //Variable that updates copyright year
-    //var year = getFullYear();
-    
-    ///////  G E T S  /////////////////////////////////////////////////
-    
-    //Homepage
-    server.get('/', function (req, res){
-        pageTitle = "MM ITC298 Library - Home";
-        res.render('home', {
-            book:books.getLibrary(),
-            pageTitle}); 
-    });
-    
-    //About Page
-    server.get('/about', function (req, res){
-        pageTitle = "Mike Murphy's ITC298 Library - About";
-        res.render('about', {pageTitle});
-    });
-    
-    //Add Book Page
-    server.get('/add', function(req, res){
-        pageTitle = "Add A Book To Murphy Library";
-        //Variable created to check for Duplicates
-       var bookMatch = books.getBook(req.params.title);
-       if (!bookMatch) {
-        bookMatch = {title: req.params.title};
-    }
-    res.render('add', {title: bookMatch} );    
-    });
-    
-    //GET DETAILS
-    server.get('/detail/:booktitle', function(req, res){
-        res.type('text/html');
-        var xBook = req.params.booktitle;
-        var bookUP = req.params.booktitle.toUpperCase();
-        var matchBook = books.getBook(xBook);
-        pageTitle = "Murphy Library - " + bookUP;
-        res.render('detail', {matchBook, pageTitle});
-    });
-
-
-    ///////////// P O S T S//////////////////////////////////////////////////
-
-    //DETAIL PAGE
-    server.post('/detail/:booktitle', function(req,res){
-    res.type('text/html');
-    //FOUND BOOK MUST COMPARE TITLES
-    var foundBook = books.getBook(req.body.bookTitle);
-    //IF BOOK IS NOT FOUND
-    if (!foundBook){
-        foundBook = {title: req.body.title};
-    }
-    res.render('detail', {books:foundBook});
-});
-
-    //ADD A BOOK
-    server.post('/add', function(req,res){
-        res.type('text/html');
-        var newBook = {"title":req.body.title.toLowerCase(), "name":req.body.name, "pages":req.body.pages, "digital":req.body.digital};
-        var result = books.addBook(newBook);
-        res.render('add', {book: newBook, result, pageTitle});
-    
-});
-
-    //REMOVE BOOK
-    server.post('/remove', function(req,res){
-       res.type('text/html'); 
-       var result = books.removeBook(req.body.title);
-       res.render('detail', {result: result});
-    });
-
-    
-    //Middleware
-
-
-    // 404 Catch-All Handler
-    server.use(function(req, res, next){
-    res.status(404);
-    res.render('404');
-});
-
-
-    //500 Error Handler
-    server.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
 
     //START SERVER
     server.listen(server.get('port'), function(){
---
+++ routes.js
@@ -0,0 +1,120 @@
+module.exports = function(server){
+ 
+   //NODE MODULE FOR LIBRARY
+   var books = require('./lib/books.js');
+
+   //TITLE TAG
+    var pageTitle = 'MM ITC298 Library';
+    
+   ///////  G E T S  /////////////////////////////////////////////////
+    
+    //Homepage
+    server.get('/', function (req, res){
+        pageTitle = "MM ITC298 Library - Home";
+        res.render('home', {
+            book:books.getLibrary(),
+            pageTitle}); 
+    });
+    
+    //About Page
+    server.get('/about', function (req, res){
+        pageTitle = "Mike Murphy's ITC298 Library - About";
+        res.render('about', {pageTitle});
+    });
+    
+    //Add Book Page
+    server.get('/add', function(req, res){
+        pageTitle = "Add A Book To Murphy Library";
+        //Variable created to check for Duplicates
+       var bookMatch = books.getBook(req.params.title);
+       if (!bookMatch) {
+        bookMatch = {title: req.params.title};
+    }
+    res.render('add', {title: bookMatch} );    
+    });
+    
+    //GET DETAILS
+    server.get('/detail/:booktitle', function(req, res){
+        res.type('text/html');
+        var xBook = req.params.booktitle;
+        var bookUP = req.params.booktitle.toUpperCase();
+        var matchBook = books.getBook(xBook);
+        pageTitle = "Murphy Library - " + bookUP;
+        res.render('detail', {matchBook, pageTitle});
+    });
+
+
+    ///////////// P O S T S//////////////////////////////////////////////////
+
+    //DETAIL PAGE
+    server.post('/detail/:booktitle', function(req,res){
+    res.type('text/html');
+    //FOUND BOOK MUST COMPARE TITLES
+    var foundBook = books.getBook(req.body.bookTitle);
+    //IF BOOK IS NOT FOUND
+    if (!foundBook){
+        foundBook = {title: req.body.title};
+    }
+    res.render('detail', {books:foundBook});
+});
+
+    //ADD A BOOK
+    server.post('/add', function(req,res){
+        res.type('text/html');
+        var newBook = {"title":req.body.title.toLowerCase(), "name":req.body.name, "pages":req.body.pages, "digital":req.body.digital};
+        var result = books.addBook(newBook);
+        res.render('detail', {book: newBook, result, pageTitle});
+    
+});
+
+    //REMOVE BOOK
+    server.post('/remove', function(req,res){
+       res.type('text/html'); 
+       var result = books.removeBook(req.body.title);
+       res.render('detail', {result: result});
+    });
+ 
+ ///////////////////////  A  P  I  ///////////////////////////////////////////////////////////
+server.get('/api/books', function(req, res){
+    var bks= books.getLibrary();
+    if(bks){
+        res.json(bks);
+    }else{
+        res.status(500).send('A Database error occurred.');
+    }
+    
+});
+
+server.get('/api/book/:booktitle', function(req,res){
+    var xTitle = req.params.booktitle.toLowerCase();
+    var xBook = books.getBook(xTitle);
+    if(xBook){
+        res.json(xBook);
+    }else{
+        res.status(404).send("No matched item");
+    }
+});
+
+ 
+ 
+ 
+ ///////////MIDDLEWARE////////////////////////////////////////
+
+
+    // 404 Catch-All Handler
+    server.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+
+    //500 Error Handler
+    server.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+ 
+ 
+    
+}
